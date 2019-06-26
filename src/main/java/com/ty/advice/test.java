9240c03b587cab4cfa3a877800d2a7dca713a33a
tianye <18629029169@163.com>
2019-06-26 22:03:42 +0800

    feat:测试推送

---
+++ src/main/java/com/ty/advice/test.java
@@ -0,0 +1,16 @@
+package com.ty.advice;
+
+/**
+ * TODO
+ *
+ * @ClassName test
+ * @Author tianye
+ * @Date 2019/6/26 22:02
+ * @Version 1.0
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("ceshi ");
+    }
+}
